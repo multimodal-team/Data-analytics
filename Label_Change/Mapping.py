ec0b9ae7efea679f374002d3895f022c5988bd26
corleone howard <mzc2218@126.com>
2019-12-13 19:38:08 +0800

    Label_Avg test

--- Label_Change/Mapping.py
+++ Label_Change/Mapping.py
@@ -11,11 +11,11 @@ for index,rows in df1.iterrows():
         df1.at[index, 'ValueExtraversion'] = '0'
     elif 0.2 < rows[1] <= 0.4:
         df1.at[index, 'ValueExtraversion'] = '1'
-    elif 0.4 < rows[1] <= 0.6:
+    elif 0.4 < rows[1] <= 0.55:
         df1.at[index, 'ValueExtraversion'] = '2'
-    elif 0.6 < rows[1] <= 0.8:
+    elif 0.55 < rows[1] <= 0.7:
         df1.at[index, 'ValueExtraversion'] = '3'
-    elif 0.8 < rows[1] <= 1:
+    elif 0.7 < rows[1] <= 1:
         df1.at[index, 'ValueExtraversion'] = '4'
 
 for index, rows in df2.iterrows():
@@ -23,15 +23,15 @@ for index, rows in df2.iterrows():
         df2.at[index, 'ValueExtraversion'] = '0'
     elif 0.2 < rows[1] <= 0.4:
         df2.at[index, 'ValueExtraversion'] = '1'
-    elif 0.4 < rows[1] <= 0.6:
-        df2.at[index, 'ValueExtraversion'] = '2'
-    elif 0.6 < rows[1] <= 0.8:
+    elif 0.4 < rows[1] <= 0.55:
+        df2.at[index, 'ValueExtraversion'] = '2'    
+    elif 0.55 < rows[1] <= 0.7:
         df2.at[index, 'ValueExtraversion'] = '3'
-    elif 0.8 < rows[1] <= 1:
+    elif 0.7 < rows[1] <= 1:
         df2.at[index, 'ValueExtraversion'] = '4'
 
 df1 = df1.loc[:,['context','ValueExtraversion']]
 df2 = df2.loc[:,['context','ValueExtraversion']]
 
-df1.to_csv('New_train.tsv', sep = '\t', index = 0)
-df2.to_csv('New_test.tsv', sep = '\t', index = 0)
+df1.to_csv('train_Unavg.tsv', sep = '\t', index = 0)
+df2.to_csv('test_Unavg.tsv', sep = '\t', index = 0)
---
+++ Label_Change/README.md
@@ -0,0 +1,28 @@
+# Data-Analytics
+Provide some useful tools for analizing data from charlearn NLP.
+
+### 12.13 Update bugs
+The testing acc is 0.2, which means original data was wrong. 
+After debuging I found csv misunderstanded the filename which prefix is '-' as a format. When did combine operations, it throw away 50 above datas.
+
+### Raw data
+Raw data which combined everything incuding 'VideoName', 'Labels', 'context'.
+TrainSet length is 6000 and corrresponed dev is 2000.
+
+
+
+
+
+---
+### Second floder
+Using huber-loss
+//TO DO 12.12 due
+
+### Personality dictionary
+//TO DO 12.12 due
+Recommended to investigate the Personality dictionary which deeps in lingustic to preduce how words affect to the personality.
+The website is 
+
+#### Early fusion
+//TO DO 12.14 due
+
